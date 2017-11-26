9b3432b59e678e9ab310213ffc95066be06297df
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-26 14:37:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,20 @@
 def prime?(num)
 
-  return false if num.even?
-  
-  i = 3
+  if num.even?
+    return false
 
-  while i <= num/3
-    
-    num.detect do 
-      if num % |i| == 0 return false 
-        else return true
-        i +=2
+  else
+
+    i = 3
+
+    while i <= num/3
+      num.detect do
+        if num % |i| == 0 return false
+          else return true
+          i +=2
+        end
       end
-    end 
+    end
     
-  end 
-  
+  end
 end
