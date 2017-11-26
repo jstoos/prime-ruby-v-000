4f4a55d727608e96ac01b27aba8b67576d3aa45b
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-26 14:38:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,8 +9,10 @@ def prime?(num)
 
     while i <= num/3
       num.detect do|i|
-        if num % i == 0 return false
-          else return true
+        if num % i == 0 
+          false
+        else 
+          true
           i +=2
         end
       end
