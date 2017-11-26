df6b7cc833d3f32a76d6be480db11a567db170dc
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-26 15:23:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -10,8 +10,9 @@ def prime?(num)
   elsif
     divisors = (3..(num-1)/2).step(2).to_a
     divisors.detect do |i|
-      if num % i == 0 return false
-        else return true
+      if num % i == 0 
+        return false
+      else return true
       end
     end
 
